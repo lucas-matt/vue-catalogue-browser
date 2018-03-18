2910a1b2176ce2f997ca459f1c7a6c9b43e4f93e
lucas-matt <lucasmatthew@hotmail.com>
2018-03-18 17:24:29 +0000

    Added home icon to navbar

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,18 +1,14 @@
 <template>
     <div id="navbar">
-        <button class="home">
-            <span class="glyphicons glyphicons-home"></span>
-        </button>
+        <icon class="home" scale="2" name="home"></icon>
     </div>
 </template>
 
 <script>
     export default {
         name: 'Navbar',
-        components: {
-        },
-        props: {
-        }
+        components: {},
+        props: {}
     }
 </script>
 
@@ -23,10 +19,15 @@
         height: 4em;
         background: linear-gradient(180deg, #555555, #000000);
         text-align: left;
+        display: flex;
     }
 
     #navbar .home {
         margin-left: 3%;
+        color: white;
+        display: block;
+        margin-top: auto;
+        margin-bottom: auto;
     }
 
 </style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,12 @@
 import Vue from 'vue'
+import 'vue-awesome/icons'
 import App from './App.vue'
+import Icon from 'vue-awesome/components/Icon'
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
+
+Vue.component('icon', Icon);
 
 new Vue({
   render: h => h(App)
-}).$mount('#app')
+}).$mount('#app');
